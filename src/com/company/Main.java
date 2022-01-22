aa5564d2d2023a9b23917f40c18cc320810fd108
Alexandr Polikarpov <nub.and.co@gmail.com>
2022-01-22 22:47:07 +0300

    первый комит

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,52 @@
 package com.company;
+import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
 
     }
+
+    public static void main(String[] args) {
+    //задание 1
+    int[] arr = generateRandomArray();
+    int spending = 0;
+    for (int a: arr) {
+        spending += a;
+    }
+        System.out.println("сумма трат за месяц составила " + spending + " рублей.");
+
+    //задание 2
+
+        int maxSpending = arr[0];
+        int minSpending = arr[0];
+        for (int b :arr) {
+            if (b > maxSpending) {
+                maxSpending = b;
+            }
+            if (b < minSpending) {
+                minSpending = b;
+            }
+        }
+
+        System.out.println("Минимальная сумма трат за день составила " + minSpending + " рублей. максимальнасумма трат за день составила " + maxSpending + " рублей.");
+    //задание 3
+        float middleSpending = spending / (float) arr.length;
+           System.out.println("средняя сумма трат за месяц составила " + middleSpending + " рублей");
+    //задание 4
+        char[] reversFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reversFullName.length -1; i >= 0; i--) {
+            System.out.print(reversFullName[i]);
+        }
+    }
 }
+
+
+
